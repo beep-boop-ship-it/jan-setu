0de4c573b98aea8e49c653683026caae5d12af16
idk-guy <226868088+beep-boop-ship-it@users.noreply.github.com>
2026-09-05 21:05:27 +0530

    Allow frontend API requests

--- backend/handlers.go
+++ backend/handlers.go
@@ -16,9 +16,17 @@ type Report struct {
 	PinCode     string `json:"pin_code"`
 	Description string `json:"description"`
 }
-
 func createReport(db *pgxpool.Pool) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
 
 		if r.Method != http.MethodPost {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
